cbd234074ac714f1a863a84ac1db05e67ce20233
leodu1228 <46442520+leodu1228@users.noreply.github.com>
2021-04-06 10:56:13 +0800

    jquery
    jquery checkbox func test

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
+const corsOptions = {
+	origin: ['http://www.example.com', 'http://localhost:3000'],
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+	allowedHeaders: [
+		'Content-Type',
+		'Authorization',
+		'Access-Control-Allow-Origin',
+	],
+};
+
+app.use(cors(corsOptions));
+
+
 app.engine('html', require('ejs').renderFile); // 使用html解析
 app.set('view engine', 'ejs'); // view engine為固定格式, 使用ejs引擎(可置換)
 
@@ -17,6 +32,10 @@ app.get('/member', function (req, res) {
 app.get('/member/:ID', function (req, res) {
 	res.render('memberprofile.html');
 });
+
+app.get('/membertest', (req, res)=>{
+	res.send('Hello Bitch!!');
+})
 
 app.listen(3000, function () {
 	console.log('This is port 3000 Bro !!');
