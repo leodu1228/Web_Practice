444e197e626019f7f31b335b6febb3d05b7411f8
leodu1228 <46442520+leodu1228@users.noreply.github.com>
2021-04-08 11:48:57 +0800

    jquery
    row / td change color

--- jquery.js
+++ jquery.js
@@ -4,81 +4,78 @@ window.onload = () => {
     const memberID = url.split("member/")[1];
     document.write("你的會員ID是  " + memberID);
   }
-  
+
   const showWord = () => {
     $("h2").html('<input />')
   }
-  
+
   const changeColor = () => {
     console.log($("#name"));
     $("#name td").css('background', 'red')
   };
-  
+
   const changeWord = (e) => {
     console.log(e.target);
     console.log(e.target.value);
     console.log($("#showWord").html()); // 顯示文字
     $("#showWord").html(e.target.value); // 改變文字
   };
-  
-  
+
+
   // Homework1 : jquery 事件綁定.on
-  $("#checkboxall").on("click change",()=>{
+  $("#checkboxall").on("click change", () => {
     $(".bubble").each((index, val) => {
       val.checked = $("#checkboxall").prop("checked");
     })
   });
-  
+
   // Homework1 : tag標籤綁定事件(addEventListener)
   checkboxtag = document.getElementById('tagcheckboxall');
-  checkboxtag.addEventListener("click", (e)=>{
+  checkboxtag.addEventListener("click", (e) => {
     bubbleArray = document.getElementsByClassName("tagbubble");
-    for(let element of bubbleArray){
+    for (let element of bubbleArray) {
       element.checked = checkboxtag.checked;
     }
   }, false)
-  
-  
+
+
   // Homework2 : 
-  $("#inputpassword").on("change", ()=>{
-  inputpassword = $("#inputpassword").val();
-  $("#confirmpassword").val(inputpassword);
+  $("#inputpassword").on("change", () => {
+    inputpassword = $("#inputpassword").val();
+    $("#confirmpassword").val(inputpassword);
   })
-  
-  
+
+
   // Homework3 : 
-  $("#inputcolor").on("change", ()=>{
-  inputcolor = $("#inputcolor").val();
-  $("#myDiv").attr("style", `background-color: ${inputcolor}`);
+  $("#inputcolor").on("change", () => {
+    inputcolor = $("#inputcolor").val();
+    $("#myDiv").attr("style", `background-color: ${inputcolor}`);
   })
 
 
 
   //Homework4 :
-  // let tb = document.getElementById("tb_color");
-  // tb.addEventListener("click", ()=>{
-  //     console.log(tb)
-  // })
-
-  let beforecolor = $("#col").css("background-color");
-
-  $("#tb").hover(
+  // 列變色
+  $("#tb tr").hover(
+    function () {
     // mouseenter
-    function(){
-      $("#col").prop("style", `background-color: coral`);
+      if ($(this).index() % 2 != 0) {
+        $(this).addClass('enterEven'); // 奇數列變色
+      } else {
+        $(this).addClass('enterOdd');  // 偶數列變色
+      }
     },
-    function(){
-      $("#col").prop("style", beforecolor);
-    }
-  ).click(()=>{
-      alert($(this).css("background-color"))
+    function () {
+    // mouseover
+      $(this).removeClass();
     }
   )
 
-
-
-
-
-
-
+  // 點擊td變色
+  $("#tb td").click(function(){
+    $(this).addClass("clickColor")
+    $(this).mouseout(function(){
+      $(this).removeClass();
+    })  
+  })
 }
